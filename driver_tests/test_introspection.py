0d97889cbcbe6f45d562d90dc8aa6a2b9ed11b55
Takayuki SHIMIZUKAWA <shimizukawa@gmail.com>
2026-08-29 23:04:19 +0900

    test: support Django 4.2 introspection selection

--- driver_tests/test_introspection.py
+++ driver_tests/test_introspection.py
@@ -9,8 +9,9 @@ from django_redshift_backend.introspection import (
 
 
 def test_internal_backend_registers_redshift_introspection():
-    assert _backend.DatabaseWrapper.introspection_class.__module__ == (
-        "django_redshift_backend.introspection"
+    assert issubclass(
+        _backend.DatabaseWrapper.introspection_class,
+        DatabaseIntrospection,
     )
 
 
