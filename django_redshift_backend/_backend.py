53a7098c5e7c76839be4779bef1bc7a3f4a15236
Takayuki SHIMIZUKAWA <shimizukawa@gmail.com>
2026-09-21 09:14:54 +0900

    fix: handle timezone checks before connecting

--- django_redshift_backend/_backend.py
+++ django_redshift_backend/_backend.py
@@ -63,6 +63,8 @@ class DatabaseWrapper(BaseDatabaseWrapper):
         return driver.connect(**conn_params)
 
     def ensure_timezone(self):
+        if self.connection is None:
+            return False
         connection_timezone_name = next(
             (
                 value.decode("ascii")
